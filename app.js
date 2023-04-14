55ef902ad63704d79f2121f2942fce32887f5f45
federico fariña <federico.farinia@gmail.com>
2023-04-14 16:05:45 -0300

    api methods into swagger and schema configuration

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cursoRoutes = require('./src/routers/curso');
+const path = require("path");
+
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node MongoDB API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: [`${path.join(__dirname, "./src/routers/*.js")}`]
+}
 
 // settings
 const app = express();
@@ -9,8 +29,8 @@ const PUERTO = process.env.PORT || 3000;
 
 //middleware
 app.use('/api', cursoRoutes);
-
 app.use(express.json());
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 
 // routers
 app.get('/', (req, res) => {
--- src/routers/curso.js
+++ src/routers/curso.js
@@ -2,10 +2,58 @@ const express = require('express');
 const cursoSchema = require("../datos/cursos");
 
 const router = express.Router();
-
 router.use(express.json());
-
 // crea un curso
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Curso:
+ *      type: object
+ *      properties:
+ *        titulo:
+ *          type: string
+ *          description: Titulo del curso
+ *        lenguaje:
+ *          type: string
+ *          description: Lenguaje que se aprenderá en el curso
+ *        vistas:
+ *          type: integer
+ *          description: Vistas totales del curso
+ *        nivel:
+ *          type: string
+ *          description: Nivel del curso
+ *      required:
+ *        - titulo
+ *        - lenguaje
+ *        - vistas
+ *        - nivel
+ *      example:
+ *        titulo: Aprender C
+ *        lenguaje: C
+ *        vistas: 1000
+ *        nivel: basico
+ */
+
+/**
+ * @swagger
+ * /api/cursos:
+ *  post:
+ *    summary: crea un nuevo curso
+ *    tags: [Curso]
+ *    requestBody:
+ *      required: true
+ *      content: 
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Curso'
+ *    responses:
+ *      200:
+ *        description: curso creado
+ *      400:
+ *        descripcion:
+ */
 router.post('/cursos', (req, res) => {
   const curso = cursoSchema(req.body);
   curso
@@ -15,6 +63,24 @@ router.post('/cursos', (req, res) => {
 })
 
 // trae todos los cursos
+/**
+ * @swagger
+ * /api/cursos:
+ *  get:
+ *    summary: retorna todos los cursos
+ *    tags: [Curso]
+ *    responses:
+ *      200:
+ *        description: retorno de todos los cursos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items: 
+ *                $ref: '#/components/schemas/Curso'
+ *      400:
+ *        descripcion:
+ */
 router.get("/cursos", (req, res) => {
   cursoSchema
     .find()
@@ -23,6 +89,30 @@ router.get("/cursos", (req, res) => {
 });
 
 // trae un curso
+/**
+ * @swagger
+ * /api/cursos/{id}:
+ *  get:
+ *    summary: retorna un curso
+ *    tags: [Curso]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema: 
+ *          type: string
+ *        required: true
+ *        description: id del curso
+ *    responses:
+ *      200:
+ *        description: retorno de todos los cursos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              
+ *      400:
+ *        descripcion: no se encontro el curso
+ */
 router.get("/cursos/:id", (req, res) => {
   const { id } = req.params;
   cursoSchema
@@ -32,6 +122,25 @@ router.get("/cursos/:id", (req, res) => {
 });
 
 // elimina un curso
+/**
+ * @swagger
+ * /api/cursos/{id}:
+ *  delete:
+ *    summary: elimina un curso
+ *    tags: [Curso]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema: 
+ *          type: string
+ *        required: true
+ *        description: id del curso
+ *    responses:
+ *      200:
+ *        description: el curso se elimino
+ *      400:
+ *        descripcion: no se encontro el curso
+ */
 router.delete("/cursos/:id", (req, res) => {
   const { id } = req.params;
   cursoSchema
@@ -41,6 +150,32 @@ router.delete("/cursos/:id", (req, res) => {
 });
 
 // modifica un curso
+/**
+ * @swagger
+ * /api/cursos/{id}:
+ *  put:
+ *    summary: modifica un curso
+ *    tags: [Curso]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema: 
+ *          type: string
+ *        required: true
+ *        description: id del curso
+ *    requestBody:
+ *      required: true
+ *      content: 
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Curso'
+ *    responses:
+ *      200:
+ *        description: el curso se modifico
+ *      400:
+ *        descripcion: no se encontro el curso
+ */
 router.put("/cursos/:id", (req, res) => {
   const { id } = req.params;
   const { titulo, lenguaje, vistas, nivel } = req.body;
