e7b0841d2da859464efb0c94aeb524563c1fed70
federico fariña <federico.farinia@gmail.com>
2023-04-14 10:45:36 -0300

    API Methods for cursos

--- src/routers/curso.js
+++ src/routers/curso.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cursoSchema = require("../datos/cursos");
 
-//const {programacion} = require('../datos/cursos.js').infoCursos;
-
 const router = express.Router();
 
 router.use(express.json());
 
+// crea un curso
 router.post('/cursos', (req, res) => {
   const curso = cursoSchema(req.body);
   curso
@@ -15,82 +14,40 @@ router.post('/cursos', (req, res) => {
   .catch((error) => res.json({ message: error}));
 })
 
-/*router.get('/', (req, res) => {
-  const curso = cursoSchema(req.body);
-    res.send(JSON.stringify(programacion));
-  });
-  
-  router.get('/:lenguaje',  (req, res) => {
-    const lenguaje = req.params.lenguaje;
-    const resultados = programacion.filter(curso => curso.lenguaje === lenguaje);
-  
-    if(resultados.length === 0){
-      return res
-      .status(404)
-      .send(`No se encontraron cursos de ${lenguaje}`);
-    }
-    if(req.query.ordenar === 'vistas'){
-      return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)));
-    }
-  
-    res.send(JSON.stringify(resultados));
-  });
-  
-  router.get('/:lenguaje/:nivel',  (req, res) => {
-    const lenguaje = req.params.lenguaje;
-    const nivel = req.params.nivel;
-    const resultados = programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
-  
-    if(resultados.length === 0){
-      return res
-      .status(404)
-      .send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
-    }
-  
-    
-  
-    res.send(JSON.stringify(resultados));
-});*/
+// trae todos los cursos
+router.get("/cursos", (req, res) => {
+  cursoSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
+// trae un curso
+router.get("/cursos/:id", (req, res) => {
+  const { id } = req.params;
+  cursoSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
+// elimina un curso
+router.delete("/cursos/:id", (req, res) => {
+  const { id } = req.params;
+  cursoSchema
+    .deleteOne({_id: id})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
-/*router.put('/:id', (req, res) => {
-    const cursoActualizado = req.body;
-    const id = req.params.id;
-
-    const indice = programacion.findIndex(curso => curso.id == id);
-
-    if(indice >= 0){
-        programacion[indice] = cursoActualizado;
-    }
-
-    res.send(JSON.stringify(programacion));
-})
-
-router.patch('/:id', (req, res) => {
-    const infoNueva = req.body;
-    const id = req.params.id;
-
-    const indice = programacion.findIndex(curso => curso.id == id);
-
-    if(indice >= 0){
-        const cursoAModificar = programacion[indice];
-        Object.assign(cursoAModificar, infoNueva);
-    }
-
-    res.send(JSON.stringify(programacion));
-})
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    const indice = programacion.findIndex(curso => curso.id == id);
-    
-    if(indice >= 0){
-        programacion.splice(indice, 1);
-    }
-
-    res.send(JSON.stringify(programacion));
-
-})*/
+// modifica un curso
+router.put("/cursos/:id", (req, res) => {
+  const { id } = req.params;
+  const { titulo, lenguaje, vistas, nivel } = req.body;
+  cursoSchema
+    .updateOne({ _id: id }, { $set: { titulo, lenguaje, vistas, nivel } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
 
 module.exports = router;
